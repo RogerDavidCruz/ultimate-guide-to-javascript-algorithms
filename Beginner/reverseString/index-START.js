c7a193560d2cd842a4d4bcb92fe9700c167e9360
Roger David Cruz <roger.david.cruz@gmail.com>
2021-07-13 22:16:31 -0400

    reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,11 +4,42 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
+// Chaining in-built methods
+
+reverseString = text => text.split('').reverse().join('')
+
+// ... With ES6 spread operator
+
+reverseString = text => [...text].reverse().join('')
+
+
+// For-Loop Way
+
+reverseString = text => {
+    let result = ""
+
+    for(let char of text) {
+        result = char + result
+    }
+
+    return result
+}
+
+// Recursive Way*
 
 function reverseString(text) {
-    // Code goes here
+    if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.substr(1)) + text[0]
+    }
 }
 
 
+// Reduce*
+
+function reverseString(text) {
+    return text.split("").reduce((acc, char) => char + acc, '')
+}
 
 module.exports = reverseString
