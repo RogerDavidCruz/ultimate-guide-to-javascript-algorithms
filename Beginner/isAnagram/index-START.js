52651fab12136e3d041edd41837014a09bc1e4c4
Roger David Cruz <roger.david.cruz@gmail.com>
2021-07-13 21:51:34 -0400

    isAnagram

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,48 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-
+//Direct Comparison
 
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    const sanitizeString = function(str) {
+        return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
+    }
+    return sanitizeString(stringA) == sanitizeString(stringB)
 }
 
+
+// module.exports = isAnagram
+
+//Character Map Comparison
+
+function isAnagram(stringA, stringB) {
+
+    function createCharMap(text) {
+        let charMap = {}
+
+        for(let char of text) {
+            if(charMap.hasOwnProperty(char)) {
+                charMap[char]++
+            } else {
+                charMap[char] = 1
+            }
+        }
+        return charMap
+    }
+
+    if (stringA.length === stringB.length) {
+        let stringAMap = createCharMap(stringA)
+        let stringBMap = createCharMap(stringB)
+
+        for (let char in stringMap) {
+            if(stringAMap[char] !== stringBMap[char]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
 
 module.exports = isAnagram
