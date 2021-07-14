583eefedb84d0378b25e0dc0b3fc2fd49f1a6229
Roger David Cruz <roger.david.cruz@gmail.com>
2021-07-14 17:23:37 -0400

    maxRecurringChar

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -4,11 +4,78 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
 
+// For...in iteration
 
-function maxRecurringChar(text) {
-    // Code goes here
+maxRecurringChar = text => {
+    let charMap = {}
+    let maxCharValue = 0
+    let maxChar = ''
+
+    for(let char of text) {
+        if(charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+    for(let char in charMap) {
+        if(charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
+
+// Forming Arrays from Character Map
+
+maxRecurringChar = text => {
+    let charMap = {}
+    let charArray = []
+    let valuesArray = []
+    let maxCharValue = 0
+
+    for (let char of text) {
+        if(charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+
+    charArray = Object.keys(charMap)
+    valuesArray = Object.values(charMap)
+    maxCharValue = Math.max(...valuesArray)
+
+
+    return charArray[valuesArray.indexOf(maxCharValue)]
+
 }
 
 
-
 module.exports = maxRecurringChar;
+
+
+/* Notes
+
+Character Mapping
+
+let charMap = {
+    a: 3,
+    b: 4,
+    c: 2,
+    d: 1
+}
+
+let charMap = {}
+for(let char of text) {
+    if(charMap.hasOwnProperty(char)) {
+        charMap[char]++
+    } else {
+        charMap[char] = 1
+    }
+}
+*/
