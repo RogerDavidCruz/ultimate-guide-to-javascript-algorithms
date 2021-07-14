09bcc23fe3e586c4986b525d806a81bd9c3317a4
Roger David Cruz <roger.david.cruz@gmail.com>
2021-07-14 18:13:36 -0400

    palindromeChecker

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,27 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
-function palindromeChecker(text) {
-    v// Code goes here
+// Reversed string (intu)
+palindromeChecker = text => {
+    let reversedText = text.toLowerCase().split('').reverse().join('')
+    return text === reversedText
 }
 
+// Looping through and comparing characters
+palindromeChecker = text => {
+    let charArray = text.toLowerCase().split('')
+    let result = charArray.every((letter, idx) => letter === charArray[charArray.length - idx - 1])
+    return result 
+}
+
+// Looping through and comparing characters (optimized)
+palindromeChecker = text => {
+    let textLen = text.length
+    for(let i = 0; i < textLen/2; i++) {
+        if(text[i] !== text[textLen - 1 - i]) return false
+    }
+    return true
+}
 
 
 module.exports = palindromeChecker;
